2d1e6763489df20e7282c4014db899e3c2fe28b1
Jonatan Holmgren <jonatan@jontes.page>
2024-05-05 20:23:50 +0200

    Expressify

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-import { Hono } from 'hono'
+import express from 'express'
 import * as Minio from 'minio'
-import { stream } from "hono/streaming"
+import sharp from 'sharp'
 
-const app = new Hono()
+const app = express()
 
 const minioClient = new Minio.Client({
-  endPoint: '10.88.0.2',
+  endPoint: '10.89.0.2',
   port: 9000,
   useSSL: false,
-  accessKey: 'ereWNoGlQcPUK6ftqCEh',
-  secretKey: 'icqXKqTcxtp6hbQHPwJPfGsRnqBvzzYKJVyIOC75',
+  // Very secure credentials 😎
+  accessKey: 'LjwAHpsnPjgkZY2BVn6H',
+  secretKey: 'smD2H1WhVdMWgm8IyM47eXah2T1HNwR7mTA8Gnvx',
 })
 
-const sizes = [256]
+const sizes = [128,256]
+
+const resizeAndUpload = (imageBuffer: ArrayBuffer, fileURL: string) => {
+  sizes.forEach(async (size) => {
+    const image = await sharp(imageBuffer).resize(size, size).webp().toBuffer()
+
+    await minioClient.putObject('ens-avatar-' + size, encodeURIComponent(fileURL), image, image.length, {
+      'Content-Type': 'image/webp'
+    })
+  })
+}
 
 const getAvatarURL = async (name: string) => {
   const response = await fetch("https://enstate.rs/n/" + name)
@@ -20,42 +31,62 @@ const getAvatarURL = async (name: string) => {
   return (data.avatar || "").toString()
 }
 
-app.get('/i/:image/:size', async (c) => {
-  if (!sizes.includes(parseInt(c.req.param().size))) {
-    return c.json({
+app.get('/:size/:image.webp', async (req, res) => {
+  console.log('Requesting image', req.params.image, 'with size', req.params.size)
+  if (!sizes.includes(parseInt(req.params.size))) {
+    res.json({
       error: 'Invalid size',
     })
   }
 
-  const bucket = 'ens-avatar-' + c.req.param().size
+  const bucket = 'ens-avatar-' + req.params.size
 
-  const fileURL = await getAvatarURL(c.req.param().image)
+  const fileURL = await getAvatarURL(req.params.image)
 
-  console.log(fileURL)
-
-  const fileStream = await minioClient.getObject(bucket, encodeURIComponent(fileURL)).catch(async (e) => {
-    if (e.code === 'NoSuchKey') {
-      const response = await fetch(fileURL)
-      const buffer = Buffer.from(await response.arrayBuffer())
-      const size = buffer.length
-      await minioClient.putObject(bucket, encodeURIComponent(fileURL), buffer, size, {
-        'Content-Type': response.headers.get('Content-Type') || 'image/webp',
-      })
-      return minioClient.getObject(bucket, encodeURIComponent(fileURL))
-    }
-  })
-
-  if (!fileStream) {
-    return c.json({
-      error: 'Failed to fetch image',
+  if (!fileURL) {
+    res.json({
+      error: 'Invalid ENS name',
     })
   }
 
-  fileStream.on("end", async () => {
-    const arrayBuffer = await fileStream.toArray()
-    return c.body(fileStream.toArray(), {
-      'Content-Type': "image/webp",
-    })
+  res.setHeader("X-Cache", "HIT")
+
+  let arrayBuffer: ArrayBuffer | undefined
+
+  const fileStream = await minioClient.getObject(bucket, encodeURIComponent(fileURL)).catch(async (e) => {
+    if (e.code === 'NoSuchKey') {
+      res.setHeader("X-Cache", "MISS")
+      console.log('File not found, downloading...')
+      const response = await fetch(fileURL)
+      
+      arrayBuffer = await response.arrayBuffer()
+
+      // Just create a bloody stream
+      return await sharp(arrayBuffer).resize(parseInt(req.params.size), parseInt(req.params.size)).webp().toBuffer()
+    }
   })
+
+  if (!fileStream || typeof fileStream === "undefined") {
+    res.json({
+      error: 'File not found',
+    })
+  }
+
+  res.setHeader('Content-Type', 'image/webp')
+  res.setHeader('Cache-Control', 'public, max-age=604800')
+
+  // Check if it's a buffer or a stream
+  if (Buffer.isBuffer(fileStream)) {
+    res.send(fileStream)
+  } else if(fileStream) {
+    fileStream.pipe(res)
+  }
+
+  if (arrayBuffer) {
+    resizeAndUpload(arrayBuffer, fileURL)
+  }
 })
-export default app
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
