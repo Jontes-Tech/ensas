c6d37838950d4104e6030c8c85995034079e63c6
Jonatan Holmgren <jonatan@jontes.page>
2024-07-08 22:28:07 +0200

    Revert magic

--- server/src/utils/populateCache.ts
+++ server/src/utils/populateCache.ts
@@ -4,7 +4,6 @@ export const populateCache = async (
     fileURL: string,
     age: number,
 ) => {
-    // If the file is older than 5 days, fetch it again
     if (age > 1000 * 60 * 60 * 24 * 5 || age === 0) {
         resizeAndUpload(fileURL);
     }
--- server/src/utils/resizeAndUpload.ts
+++ server/src/utils/resizeAndUpload.ts
@@ -2,28 +2,24 @@ import mq from 'amqplib/callback_api';
 
 let channel: mq.Channel | null = null;
 
-const connectRabbitMQ = (): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    mq.connect(process.env.AMQP || "amqp://localhost", (err, conn) => {
-      if (err) reject(err);
-      conn.createChannel((err, ch) => {
-        if (err) reject(err);
-        ch.assertQueue('ensas_queue');
-        channel = ch;
-        resolve();
-      });
+const connectRabbitMQ = () => {
+  mq.connect(process.env.AMQP || "amqp://localhost", (err, conn) => {
+    if (err) throw err;
+    conn.createChannel((err, ch) => {
+      if (err) throw err;
+      ch.assertQueue('ensas_queue');
+      channel = ch;
     });
   });
 }
 
-export const getChannel = async (): Promise<mq.Channel | null> => {
+export const getChannel = () => {
   if (!channel) {
-    await connectRabbitMQ();
+    connectRabbitMQ();
   }
   return channel;
 }
 
 export const resizeAndUpload = async (file: string) => {
-    const channel = await getChannel();
-    channel?.sendToQueue('ensas_queue', Buffer.from(file));
+    getChannel()?.sendToQueue('ensas_queue', Buffer.from(file));
 }
